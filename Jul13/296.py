ca456e1a810b336ad8a9d50ec4a09626d8ca3238
Sofia Kurochkina <sofikurochkina@gmail.com>
2022-07-13 23:04:48 +0200

    while loops

---
+++ Jul13/296.py
@@ -0,0 +1,15 @@
+num = int(input())
+
+kot  = 0
+lisa = 0
+
+# платить трехрублёвыми пока не будет сумма которая делится на 5 на цело 
+
+while num%5 !=0:
+    num -=3 
+    lisa +=1
+
+kot = num //5 
+
+
+print (kot, lisa)
