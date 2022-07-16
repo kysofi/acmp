9f8b45fbf9b19ea11b7b657eeda85777f8dcd6e8
Sofia Kurochkina <sofikurochkina@gmail.com>
2022-07-16 23:03:03 +0200

    sorting

---
+++ Jul16/1168.py
@@ -0,0 +1,20 @@
+counts = list(map(int,input().split()))
+N = int(counts[0]) # количество абитуриетов 
+M = int(counts[1]) # количество абитуриентов, которые будут зачислены
+A = []
+for i in range (N):
+    A.append(input().split())
+
+sum = 0
+
+for i in range (N):
+    sum = int(A[i][1])+int(A[i][2])+int(A[i][3])
+    A[i].append(sum)
+
+for run in range (N-1):
+    for j in range (0,N-1-run):
+        if A[j+1][4]>A[j][4]:
+            A[j],A[j+1]=A[j+1],A[j]
+
+print (A)
+
