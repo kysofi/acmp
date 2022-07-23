0401d87484583cf684e0d868f3217b8ff28e74bc
Sofia Kurochkina <sofikurochkina@gmail.com>
2022-07-24 00:09:38 +0200

    reversed

--- Jul23/1219.py
+++ Jul23/1219.py
@@ -40,10 +40,8 @@ list1=[]
 for x in range(n+1):
     list1.append(x)
 
-print(list1[b:a-1:-1])
-
-list1[a:b+1]=list1[b:a-1:-1]
-list1[c:d+1]=list1[d:c-1:-1]
+list1[a:b+1]=list1[a:b+1][::-1]
+list1[c:d+1]=list1[c:d+1][::-1]
 
 print(*list1[1:])
 
