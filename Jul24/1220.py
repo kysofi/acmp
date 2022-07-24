d4a6eb3e40296148252d07a6cca5fdbf10c985f4
Sofia Kurochkina <sofikurochkina@gmail.com>
2022-07-24 22:14:53 +0200

    changes

--- Jul24/1220.py
+++ Jul24/1220.py
@@ -2,7 +2,6 @@ N = int(input())
 nums = list(map(int,input().split()))
 K = int(input())
 
-
 if K == 0: 
     nums = nums
 
@@ -11,20 +10,6 @@ if abs(K)>N:
         K = (K%N) * (-1)
     else:
         K = K%N
-
-""" 
-if K > 0: 
-    for i in range(K):
-        nums.insert(0,nums.pop(-1))
-
-if K < 0:
-    for i in range(abs(K)):
-        nums.append(nums.pop(0))
-"""
-#print(*nums)
-
-# nums[-3:] >> last three
-# nums[:3] >> first three
 
 if K > 0: 
     answer = nums[-K:] + nums[:(N-K)]
@@ -35,3 +20,14 @@ if K < 0:
 
 print(*answer)
 
+""" 
+if K > 0: 
+    for i in range(K):
+        nums.insert(0,nums.pop(-1))
+        # pop last, insert to the beginning
+
+if K < 0:
+    for i in range(abs(K)):
+        nums.append(nums.pop(0))
+        # pop first, append to the end
+"""
