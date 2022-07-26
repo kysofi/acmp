7ad2c4190c012e8c867e248eea258e3baebacd7a
Sofia Kurochkina <sofikurochkina@gmail.com>
2022-07-26 22:45:03 +0200

    functions-2

--- Jul25/1080.py
+++ Jul25/1080.py
@@ -1,47 +1,49 @@
 N = int(input()) # length of the list
 nums = list(map(int,input().split())) # numbers
 M = int(input()) # number of operations 
+
 operations = []
 
 for i in range (M):
     operations.append(list(input().split()))
 
-for i in range (M):
+def get(i):  # this
+    print(nums[(int(i)-1)])
+    return nums
 
-    #for j in range (0,10):
+def update(i,j,k):
+    for z in range ((int(i)-1),int(j)):
+        nums[z] = (int(k))
+    return nums
 
-    if operations[i][0] == 'get': # this 
+def add(i,j,k):
+    for z in range ((int(i)-1),int(j)):
+        nums[z] += (int(k))
+    return nums
 
-        print(nums[((int(operations[i][1])-1))])
+def rsq(i,j): # this
+    sum = 0
+    for z in range ((int(i)-1),int(j)):
+        sum += nums[z] 
+    print(sum)
+    return nums
+    
+def rmq(i,j): # this
+    minimum = ((int(i)-1))
+    for z in range ((int(i)-1),int(j)):
+        minimum=min(nums[z],minimum)
+    print(minimum)
+    return nums
+    
+for num in range(M):
 
-        #print('nums',nums)
-
-    elif operations[i][0] == 'update':
-
-        for j in range ((int(operations[i][1])-1),int(operations[i][2])):
-            nums[j] = int(operations[i][3])
-        #print('nums',nums)
-
-    elif operations[i][0] == 'add':
-
-        for j in range ((int(operations[i][1])-1),int(operations[i][2])):
-            nums[j] = int(nums[j]) + int(operations[i][3])
-        #print('nums',nums)
-
-    elif operations[i][0] == 'rsq': # this 
-
-        sum = 0
-        for j in range ((int(operations[i][1])-1),int(operations[i][2])):
-            sum += nums[j]
-        print(sum)
-        #print('nums',nums)
-
-    elif operations[i][0] == 'rmq': # this 
-
-        minimum = ((int(operations[i][1])-1))
-        for j in range ((int(operations[i][1])-1),int(operations[i][2])):
-            if nums[j]< minimum:
-                minimum=nums[j]
-        print(minimum)
-        #print('nums',nums)
-
+    if operations[num][0]=='get':
+        get(operations[num][1])
+    elif operations[num][0]=='update':
+        update(operations[num][1],operations[num][2],operations[num][3])
+    elif operations[num][0]=='add':
+        add(operations[num][1],operations[num][2],operations[num][3])
+    elif operations[num][0]=='rsq':
+        rsq(operations[num][1],operations[num][2])
+    elif operations[num][0]=='rmq':
+        rmq(operations[num][1],operations[num][2])
